568713013c6c38d6e664f6683a1951e326be245d
Amardeep Phule <amardeepphule@gmail.com>
2022-09-08 23:18:21 +0530

    feat: overlay cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  };
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
   return (
     <div>
-      <Header />
-      <Meals />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onClick={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import classes from "./Header.module.css";
 import mealsImage from "../../asset/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h2>Food Meals</h2>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onClick} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Delicious food on table" />
