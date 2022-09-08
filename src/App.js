e5ba8558cf1f5ee0c1a631ebd5a9efe163e65203
Amardeep Phule <amardeepphule@gmail.com>
2022-09-08 19:21:56 +0530

    feat: food dummy list with add quantity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <div>
       <Header />
+      <Meals />
     </div>
   );
 }
